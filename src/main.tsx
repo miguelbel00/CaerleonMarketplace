d081f5363ed84021881c2d1899d0cdf398a5f4a4
heratok <goku55673@gmail.com>
2023-12-19 19:25:30 -0500

    add routes,footer and navbar

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './Home.tsx'
+import Home from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
