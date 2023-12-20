e376f2c70ef32d5ee3949ef0b6c6ca0c089e2257
miguelbel00 <miguelabeltranp@gmail.com>
2023-12-19 19:58:42 -0500

    fix merge

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-<<<<<<< HEAD
-import Home from './App.tsx'
-=======
 import Home from './Home.tsx'
 import PricesManager from './components/PricesManager.tsx'
->>>>>>> 8d8d37368aa30b3c33841d7cab9c988195c26fa0
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
